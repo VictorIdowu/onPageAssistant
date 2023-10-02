a24fd4cf66e30069af57ed8efff0b4811e2609c3
VictorIdowu <idsonvictor@gmail.com>
2023-10-02 17:37:46 +0100

    Added Nav and nav function

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,8 @@ function App() {
     const animationBottom = animation.offsetTop - animation.offsetHeight;
     const elBottom = el.offsetTop - el.offsetHeight;
 
+    animation.style.opacity = "100%";
+
     animation.style.transform = `translate(${
       el.offsetLeft - animation.offsetLeft + animation.offsetHeight * 2
     }px, -${
@@ -38,7 +40,7 @@ function App() {
   // Move animation to text
   const moveToText = async (e) => {
     const el = await e.target;
-    getPosition(el);
+    await getPosition(el);
     setMsg("This text describes what the business does");
   };
 
@@ -52,25 +54,45 @@ function App() {
     }, 3000);
   };
 
+  // Move animation to nav
+  const moveToNav = async (e) => {
+    const el = await e.target;
+    await getPosition(el);
+    setMsg(`Link on this text to navigate to the ${e.target.textContent} page`);
+  };
+
   // Move animation back
-  const moveBack = () => {
+  const moveBack = async () => {
     animation.style.transform = `translate(${animation.offsetLeft}px)`;
+    setTimeout(() => {
+      animation.style.opacity = "0";
+    }, 1000);
     setMsg("");
   };
 
   return (
     <section className="w-full">
-      <header>
-        <h2>Logo</h2>
-      </header>
+      <nav className="absolute top-0 left-0 right-0 z-30 flex justify-center text-gray-100 p-3 md:p-5 gap-6">
+        {["About Us", "Contact Us", "Blogs"].map((item, i) => (
+          <a
+            onMouseOver={moveToNav}
+            onMouseLeave={moveBack}
+            className="hover:border-b-[1px] border-gray-300 ease-in duration-300"
+            key={i + 1}
+            href="#"
+          >
+            {item}
+          </a>
+        ))}
+      </nav>
       <img
         className="abolute top-0 w-full h-[80vh] brightness-[0.3]"
         src="https://aofund.org/app/uploads/2022/09/business-grants-for-black-women-scaled.jpg"
         alt=""
       />
 
-      <aside className="absolute top-1/2 left-0 -translate-y-1/2 grid grid-rows-2 md:grid-cols-2  gap-3">
-        <div className="  flex flex-col gap-4 pl-3">
+      <aside className="absolute top-1/2 left-0 -translate-y-1/2 grid grid-rows-2 md:grid-cols-2 md:mt-12 pl-10 ">
+        <div className="  flex flex-col gap-10 pl-3">
           <h2
             className="text-gray-100 text-xl sm:text-4xl"
             onMouseOver={moveToText}
@@ -92,20 +114,20 @@ function App() {
             onClick={moveToButton}
             className="py-2 px-4 text-gray-200 text-base sm:text-xl bg-gray-700 rounded-lg w-fit"
           >
-            Info Button
+            Get Started
           </button>
         </div>
         <div className="text-gray-100 flex flex-col gap-3"></div>
       </aside>
       <div
         data-testid="animation"
-        className={`flex flex-col items-center sm:text-start  rounded-[50%] absolute left-5 bottom-5 ease-in duration-1000`}
+        className={`flex flex-col opacity-0 sm:text-start  rounded-[50%] absolute left-5 bottom-5 transition-all ease-in duration-1000`}
       >
         <Icon
           className="text-6xl sm:text-8xl"
           icon="fluent-emoji-flat:girl-medium"
         />
-        <p className="bg-green-400 text-gray-100 px-3 rounded-3xl type">
+        <p className="bg-green-900 text-gray-100 font-semibold px-6 py-3 rounded-3xl type">
           {msg}
         </p>
       </div>
